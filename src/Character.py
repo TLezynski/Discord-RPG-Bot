cd1ffa0174ef08a6c2b76ea22d6a7249b499fcaa
Trevor Lezynski <tlezynski@gmail.com>
2018-11-20 13:21:48 -0500

    Adding functionality slowly... Need to figure out the "level" dilemna

--- src/Character.py
+++ src/Character.py
@@ -3,6 +3,8 @@ class Character:
         self.name = name
         self.level = 1
         self.xp = 0
+        self.max_hp = 0
+        self.curr_hp = 0
 
     def getName(self):
         return self.name
@@ -13,5 +15,23 @@ class Character:
     def getXP(self):
         return self.xp
 
+    def setMaxHP(self, n):
+        self.max_hp = n
+
+    def heal(self, n):
+        if(self.curr_hp + n > self.max_hp):
+            self.curr_hp = self.max_hp
+        else:
+            self.curr_hp += n
+
+    def goingToLevelUp(self, n):
+        if(self.xp + n > 1000*self.level):
+            return True
+        else:
+            return False
+
     def addXP(self, n):
-        self.xp += n
+        if(self.goingToLevelUp(self, n)):
+            self.level += 1
+        else:
+            self.xp += n
--- src/Wizard.py
+++ src/Wizard.py
@@ -1,7 +1,10 @@
-from src import Character
+from src.Character import Character
 
 
 class Wizard(Character):
 
     def __init__(self, name):
         Character.__init__(self, name)
+        self.max_damage = 10
+        self.min_damage = 5
+        Character.setMaxHP(self, 15)
