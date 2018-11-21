f196f23992189d3878f69137f0c1ac37526264e3
Trevor Lezynski <tlezynski@gmail.com>
2018-11-20 23:08:52 -0500

    Added TODO on Wizard, will revisit

--- src/Wizard.py
+++ src/Wizard.py
@@ -5,6 +5,10 @@ class Wizard(Character):
 
     def __init__(self, name):
         Character.__init__(self, name)
-
         Character.setMaxHP(self, 15)
-        Character.set
+        Character.setMaxDamae(self, 10)
+        Character.setMinDamage(self, 5)
+
+    # TODO
+    def levelUp(self):
+        return
