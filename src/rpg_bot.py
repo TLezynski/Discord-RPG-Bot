9f5277a256643fdba9c5e577b22d63bff334bd4b
Trevor Lezynski <tlezynski@gmail.com>
2019-09-03 10:17:23 -0400

    Program stable, new features and bugs need to be added

--- src/rpg_bot.py
+++ src/rpg_bot.py
@@ -1,5 +1,4 @@
 # Work with Python 3.6
-import discord
 import sys
 import random
 from src.Healer import Healer
@@ -7,13 +6,13 @@ from src.DPS import DPS
 from src.Tank import Tank
 from src.CharacterDB import CharacterDB
 from discord.ext import commands
-from discord.ext.commands import Bot
 
-BOT_PREFIX = ("!")
+BOT_PREFIX = "!"
 TOKEN = sys.argv[1]
 characterDB = CharacterDB()
 
 bot = commands.Bot(command_prefix=BOT_PREFIX)
+
 
 def trimContext(context):
     # Trim the front off of the context and return the remainder of the string
@@ -28,7 +27,7 @@ def trimContext(context):
 @bot.command(name="hello", brief="Say hello to the bot!", pass_context=True)
 async def hello(context):
     possible_responses = ["Hello", "Greetings", "Well met", "Hi there", "What's up"]
-    await bot.say(random.choice(possible_responses) + " " + context.message.author.mention)
+    await context.send(random.choice(possible_responses) + " " + context.message.author.mention)
 
 
 @bot.command(name="newCharacter",
@@ -39,51 +38,48 @@ async def hello(context):
 async def newCharacter(context):
     # Break off thee command text from the message, make sure it's valid
     string = trimContext(context)
-    if(len(string) == 0):
-        await bot.say("`!newCharacter <name>, <Healer, Tank, DPS>`")
-
     nameAndRole = string.split(",")
 
     # Error check the list to make sure it doesn't have a comma
-    if(len(nameAndRole) != 2):
-        await bot.say("`!newCharacter <name>, <Healer, Tank, DPS>`")
+    if(len(string) == 0 or len(nameAndRole) != 2):
+        return await context.send("`!newCharacter <name>, <Healer, Tank, DPS>`")
 
     name = nameAndRole[0]
     role = nameAndRole[1].lower()
 
     # Error check the name to make sure it's not super long
     if(len(name) > 16):
-        await bot.say("`Your character's name must not exceed 16 characters`")
+        return await context.send("`Your character's name must not exceed 16 characters`")
 
     # Error check the name to make sure it's only characters
     elif(not name.isalpha()):
-        await bot.say("`Your character's name must contain only letters`")
+        return await context.send("`Your character's name must contain only letters`")
 
     # On valid input, make a character based on their role
     else:
         if(characterDB.isInDB(name)):
-            return await(bot.say("`There already exists a character with that name. Please choose another`"))
+            return await(context.send("`There already exists a character with that name. Please choose another`"))
 
         if(role == "dps"):
             # Make a DPS character
             char = DPS(name)
             characterDB.addToDB(char)
-            return await bot.say("`" + name + " created successfully, Role: DPS`")
+            return await context.send("`" + name + " created successfully, Role: DPS`")
 
         elif(role == "healer"):
             # Make a healer character
             char = Healer(name)
             characterDB.addToDB(char)
-            return await bot.say("`" + name + " created successfully, Role: Healer`")
+            return await context.send("`" + name + " created successfully, Role: Healer`")
 
         elif (role == "tank"):
             # Make a tank character
             char = Tank(name)
             characterDB.addToDB(char)
-            return await bot.say("`" + name + " created successfully, Role: Tank`")
+            return await context.send("`" + name + " created successfully, Role: Tank`")
 
         else:
-            return await bot.say("`" + role + " is not an acceptable role. Please choose DPS, Healer, or Tank.`")
+            return await context.send("`" + role + " is not an acceptable role. Please choose DPS, Healer, or Tank.`")
 
 
 @bot.command(name="stats",
@@ -94,12 +90,12 @@ async def newCharacter(context):
 async def stats(context):
     name = trimContext(context)
     if(len(name) == 0):
-        return await bot.say("`No name was entered`")
+        return await context.send("`No name was entered`")
 
     if(not characterDB.isInDB(name)):
-        return await bot.say("`Character '" + name + "' was not found in the database.`")
+        return await context.send("`Character '" + name + "' was not found in the database.`")
 
-    return await bot.say(characterDB.getCharacter(name).toString())
+    return await context.send(characterDB.getCharacter(name).toString())
 
 @bot.event
 async def on_ready():
